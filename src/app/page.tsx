f3907047b6ca8e6cff7a92b1bf74d7007b0ceb87
sana arshad <160604463+Aaira123@users.noreply.github.com>
2024-11-10 03:20:28 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <h1 className=" flex justify-center items-center  hover:text-green-700 py-6 text-7xl text-red-900">Hello World!!!</h1>
